73ac91d000ced3fcb554f0fff00253cd3537283c
Guilherme Souza <guilhermematos2313@gmail.com>
2025-08-11 15:54:37 -0300

    Vet training

---
+++ src/vetoresUnidimensionais/Ex01MaiorMenorValor.java
@@ -0,0 +1,41 @@
+/*
+Crie um programa que:
+Receba os preços de fechamento de uma ação durante 10 dias.
+
+Use os métodos:
+
+maiorPreco(double[] precos) → retorna o maior preço.
+
+
+menorPreco(double[] precos) → retorna o menor preço.
+*/
+package vetoresUnidimensionais;
+
+public class Ex01MaiorMenorValor {
+
+    public static double maiorPreco(double[] precos){
+        double maiorPreco = precos[0];
+        for(double p : precos){
+            if(p>maiorPreco){
+                maiorPreco = p;
+            }
+        }
+        return maiorPreco;
+    }
+
+    public static double menorPreco(double[] precos){
+        double menorPreco = precos[0];
+        for(double p : precos){
+            if(p<menorPreco){
+                menorPreco = p;
+            }
+        }
+        return menorPreco;
+    }
+
+    public static void main(String[] args) {
+        double[] vet = {100,200,300,400};
+        System.out.println(maiorPreco(vet));
+        System.out.println(menorPreco(vet));
+    }
+}
