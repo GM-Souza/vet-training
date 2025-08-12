5a9dd8730aefa5248da65dfe9209a05a2991e281
Guilherme Souza <guilhermematos2313@gmail.com>
2025-08-12 16:32:58 -0300

    Ex03 - VetoresBidimensionaisBasicos - finished

--- src/VetoresBidimensionaisBasicos/Ex03SaldoTotalEmedia.java
+++ src/VetoresBidimensionaisBasicos/Ex03SaldoTotalEmedia.java
@@ -1,26 +1,42 @@
 package VetoresBidimensionaisBasicos;
 
+import java.util.Arrays;
+
 public class Ex03SaldoTotalEmedia {
 
    public static double calcularSaldoTotal(double[][] matriz){
-       double somaLinha = 0;
+       double somaTotal = 0;
 
        for (int i = 0; i < matriz.length; i++) {
+           somaTotal = 0;
            for (int j = 0; j < matriz[i].length; j++){
-               somaLinha = somaLinha + matriz[i][j];
+               somaTotal += matriz[i][j];
            }
-       }
-       return somaLinha;
+           System.out.print(somaTotal+ " ");
+       }return somaTotal;
+    }
+    public static double[] calcularMediaPorCliente(double[][] matriz){
+            double[] result = new double[matriz.length];
+
+        for (int i = 0; i < matriz.length; i++) {
+            double media = 0;
+            for (int j = 0; j < matriz[i].length; j++){
+                media += matriz[i][j]/matriz[j].length;
+                result[i] = media;
+            }
+        }return result;
     }
 
     public static void main(String[] args) {
 
         double[][] mat = {
-                {100, 2},
-                {120, 2},
-                {130, 2},
+                {1000, 1500},
+                {500, 700},
+                {2000, 1800},
         };
 
-        System.out.println(calcularSaldoTotal(mat));
+        System.out.println(Arrays.toString(calcularMediaPorCliente(mat)));
+        calcularSaldoTotal(mat);
+
     }
 }
