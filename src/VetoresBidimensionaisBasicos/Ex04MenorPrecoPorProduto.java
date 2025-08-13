e448f66d94fd5816623f1833ab8cd857b5e4b78b
GM-Souza <guilhermematos2313@gmail.com>
2025-08-13 08:13:09 -0300

    Ex04 - VetoresBidimensionaisBasicos - Fix

--- src/VetoresBidimensionaisBasicos/Ex04MenorPrecoPorProduto.java
+++ src/VetoresBidimensionaisBasicos/Ex04MenorPrecoPorProduto.java
@@ -5,22 +5,25 @@ import java.util.Arrays;
 public class Ex04MenorPrecoPorProduto {
 
     public static double[] encontrarMenorPrecoPorProduto(double[][] matriz){
-        double[] vet = new double[matriz.length];
+        double[] vet = new double[matriz[0].length];
 
-        for (int i = 0; i < matriz.length; i++) {
-            double menor = matriz[i][0];
-            for(int j = 0; j< matriz[i].length; j++){
+        for (int j = 0; j < matriz[0].length; j++) {
+            double menor = matriz[0][j];
+            for(int i = 1; i< matriz.length; i++){
                 if(matriz[i][j] < menor){
                     menor = matriz[i][j];
                 }
             }
-            vet[i] = menor;
+            vet[j] = menor;
         }
         return vet;
     }
     public static void main(String[] args) {
 
-        double[][] mat = {{10,12,9},{13,24,10}};
+        double[][] mat = {
+                {10,12,9},
+                {13,24,10}
+        };
 
         System.out.println(Arrays.toString(encontrarMenorPrecoPorProduto(mat)));
     }
